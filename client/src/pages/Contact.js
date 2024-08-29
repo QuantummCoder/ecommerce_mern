2b371a50cfdb9c6d1a2480b616c2a97b9afcb780
QunatummCoder <abhinav.3721@gmail.com>
2024-08-29 20:16:56 +0530

    updated contact and policy page

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -3,8 +3,14 @@ import Layout from '../components/Layout/Layout'
 
 const Contact = () => {
   return (
-    <Layout>
-        <h1>Contact page</h1>
+    <Layout title={"contact-us"}>
+        <div className='contact'>
+        <div>
+          <h1>Get in Touch</h1>
+          <p>Want to get in touch?? We'd love to hear from you. Here's how you can reach us...</p>
+        </div>
+        <img src='/images/contact.jpeg' alt='contact' />
+        </div>
     </Layout>
   )
 }
--- client/src/pages/Policy.js
+++ client/src/pages/Policy.js
@@ -3,8 +3,24 @@ import Layout from '../components/Layout/Layout'
 
 const Policy = () => {
   return (
-    <Layout>
-        <h1>Policy Page</h1>
+    <Layout title={"Policy page"}>
+      <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/images/contactus.jpeg"
+            alt="contactus"
+            style={{ width: "100%",margin: "10px"}}
+          />
+        </div>
+        <div className="col-md-4" style={{marginTop: "15px", fontFamily: "'Poppins', sans-serif", fontSize: "1rem"}}>
+          <p>Privacy Policy: Outlines data collection, usage, protection, user rights, and third-party sharing practices.</p>
+          <p>Return and Refund Policy: Defines return eligibility, process, refund timelines, non-returnable items, and shipping cost responsibilities.</p>
+          <p>Shipping Policy: Details shipping options, delivery times, costs, handling time, and order tracking.
+          </p>
+          <p>Terms of Service: Sets user responsibilities, prohibited activities, intellectual property rights, and service limitations.</p>
+          <p>Cookie Policy: Explains cookie usage, types, purposes, user management options, and privacy impact.</p>
+        </div>
+      </div>
     </Layout>
   )
 }
